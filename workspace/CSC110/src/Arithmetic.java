f380345e1aac0b48886ac4bcb4eb729017a06bb0
ebawolek <synapsestudios@Synapses-Mac-mini.local>
2017-02-06 14:28:05 -0700

    Update Arithmetic

--- workspace/CSC110/src/Arithmetic.java
+++ workspace/CSC110/src/Arithmetic.java
@@ -10,42 +10,44 @@
    n minus the last digit of n is y
    the sum of n and n+1 and n+2 is y
  */
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.io.InputStream;
+
 public class Arithmetic {
 
 	public static void main(String[] args) { //trying to get exception handling for arthimetic.
 		Scanner scan = new Scanner(System.in);
-		while(true)
-		{
+		while(true){
 			System.out.print("Enter an integer: ");
-		}
+			}
 		try 
 		{
 			double n = scan.nextDouble();
-			return n;
+			double half, squared, recip, tenth, tenthsq, sum,opp, twice, minus;
+			opp = -n;
+			twice = n *2;
+			half = n/2; 
+			squared = Math.pow(n, 2);
+			recip = 1/n;
+			tenth = n *.1;
+			tenthsq = Math.pow(tenth, 2);
+			sum = n + (n+1) + (n+2);
+			
+			System.out.println("The opposite of "+n+" is "+ opp);
+			System.out.println(n+ "dobuled is "+twice);
+			System.out.println("one-half of "+n+" is "+ half);
+			System.out.println("The reciprocal of "+n+" is "+recip);
+			System.out.println("one-tenth of "+n+ " is "+tenth+" and "+ tenth+ " squared is "+ tenthsq);
+			System.out.println("The sum of "+n+ " and "+ (n+1) +" and "+(n+2) +" is "+ sum);
+
 		}
-		catch (java.util.InputMistmatchException e)
+		catch ( InputMismatchException e)
 		{
-			scan.nextLine();
+			System.out.println("Please input a number");//Process ends.
 		}
-		double half, squared, recip, tenth, tenthsq, sum,opp, twice, minus;
 		
-		opp = -n;
-		twice = n *2;
-		half = n/2; 
-		squared = Math.pow(n, 2);
-		recip = 1/n;
-		tenth = n *.1;
-		tenthsq = Math.pow(tenth, 2);
-		sum = n + (n+1) + (n+2);
 		
-		System.out.println("The opposite of "+n+" is "+ opp);
-		System.out.println(n+ "dobuled is "+twice);
-		System.out.println("one-half of "+n+" is "+ half);
-		System.out.println("The reciprocal of "+n+" is "+recip);
-		System.out.println("one-tenth of "+n+ " is "+tenth+" and "+ tenth+ " squared is "+ tenthsq);
-		System.out.println("The sum of "+n+ " and "+ (n+1) +" and "+(n+2) +" is "+ sum);
 		
 		//I want to now practice control versioning
 		
