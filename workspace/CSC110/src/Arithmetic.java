bf7aa9375a2b62d63c4f760f672d0dad48832de1
ebawolek <synapsestudios@Synapses-Mac-mini.local>
2017-02-06 15:02:14 -0700

    Updated Arithmetic

--- workspace/CSC110/src/Arithmetic.java
+++ workspace/CSC110/src/Arithmetic.java
@@ -18,39 +18,37 @@ public class Arithmetic {
 
 	public static void main(String[] args) { //trying to get exception handling for arthimetic.
 		Scanner scan = new Scanner(System.in);
-		while(true){
-			System.out.print("Enter an integer: ");
+		while(true)//loop infinitely until a correct input
+		{	
+			try 
+			{	
+				System.out.print("Enter an integer: ");
+				double n = scan.nextDouble();
+				double half, squared, recip, tenth, tenthsq, sum,opp, twice, minus;
+				opp = -n;
+				twice = n *2;
+				half = n/2; 
+				squared = Math.pow(n, 2);
+				recip = 1/n;
+				tenth = n *.1;
+				tenthsq = Math.pow(tenth, 2);
+				sum = n + (n+1) + (n+2);
+				
+				System.out.println("The opposite of "+n+" is "+ opp);
+				System.out.println(n+ "dobuled is "+twice);
+				System.out.println("one-half of "+n+" is "+ half);
+				System.out.println("The reciprocal of "+n+" is "+recip);
+				System.out.println("one-tenth of "+n+ " is "+tenth+" and "+ tenth+ " squared is "+ tenthsq);
+				System.out.println("The sum of "+n+ " and "+ (n+1) +" and "+(n+2) +" is "+ sum);
+				break;//if no exception the loop breaks
 			}
-		try 
-		{
-			double n = scan.nextDouble();
-			double half, squared, recip, tenth, tenthsq, sum,opp, twice, minus;
-			opp = -n;
-			twice = n *2;
-			half = n/2; 
-			squared = Math.pow(n, 2);
-			recip = 1/n;
-			tenth = n *.1;
-			tenthsq = Math.pow(tenth, 2);
-			sum = n + (n+1) + (n+2);
-			
-			System.out.println("The opposite of "+n+" is "+ opp);
-			System.out.println(n+ "dobuled is "+twice);
-			System.out.println("one-half of "+n+" is "+ half);
-			System.out.println("The reciprocal of "+n+" is "+recip);
-			System.out.println("one-tenth of "+n+ " is "+tenth+" and "+ tenth+ " squared is "+ tenthsq);
-			System.out.println("The sum of "+n+ " and "+ (n+1) +" and "+(n+2) +" is "+ sum);
-
-		}
-		catch ( InputMismatchException e)
-		{
-			System.out.println("Please input a number");//Process ends.
-		}
-		
-		
-		
-		//I want to now practice control versioning
-		
+			catch ( InputMismatchException e)
+			{
+				System.out.println("Please input a number");
+				scan.next();//clears the scanner of the wrong input
+				continue;//continue to loop if exception is found
+			}
+		}	
 	}
 
 }
